b67c18dbe9c7ce7306422252a34b7c44c09ff49a
Unknown <laurent.sa@outlook.fr>
2018-08-09 12:51:12 +0200

    remove old button and add left right arrow with font awesom

--- slider.js
+++ slider.js
@@ -5,32 +5,52 @@ var slider = {
         "images/bg1.jpg",
         "images/bg2.jpg",
     ],
-    parent : "#slider",
+    parent : "header",
     alt : "image du slider",
     width : "100%",
     height : "500px",
 
+    
+
     init : function(){
+
+        //creation de l'element contenant le slider
+        var slider = document.createElement("div");
+        slider.id = "slider";
+        slider.style.color = "white";
+        slider.style.fontSize = "3em";
+        slider.width = this.width;
+        slider.style.height = this.height;
+        document.querySelector(this.parent).appendChild(slider);
 
         //creation de la balise <img>
         var image = document.createElement("img");
-        document.querySelector(this.parent).appendChild(image);
+        document.querySelector("#slider").appendChild(image);
         image.src = this.tableauImage[this.index];
         image.alt = this.alt;
         image.style.width = this.width;
         image.style.height = this.height;
 
-        //bouton gauche droit
-        var bouton = document.createElement("button");
-        bouton.id = "gauche";
-        bouton.innerHTML = "precedent";
-        document.querySelector(this.parent).appendChild(bouton);
+        //creation chevron gauche
+        var gauche = document.createElement("i");
+        gauche.id = "gauche";
+        gauche.style.position = "relative";
+        gauche.style.left = "4%";
+        gauche.style.bottom = "280px";
+        gauche.style.padding = "10px";
+        gauche.className = "fas fa-chevron-left";
+        document.querySelector("#slider").appendChild(gauche);
 
-        var bouton = document.createElement("button");
-        bouton.id = "droit";
-        bouton.innerHTML = "suivant";
-        document.querySelector(this.parent).appendChild(bouton);
-
+        //creation chevron droit
+        var droit = document.createElement("i");
+        droit.id = "droit";
+        droit.style.position = "relative";
+        droit.style.left = "90%";
+        droit.style.bottom = "280px";
+        droit.style.padding = "10px";
+        droit.className = "fas fa-chevron-right";
+        document.querySelector("#slider").appendChild(droit);
+        
         this.slideDroit(image);
         this.slideGauche(image);
         this.toucheClavier(image);
